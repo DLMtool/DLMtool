b821645293478642dffc0041f7263411bcb675b0
Adrian Hordyk <ar.hordyk@gmail.com>
2017-03-23 15:06:35 +0800

    update runMSErobust function with additional messages

--- R/runMSErobust.r
+++ R/runMSErobust.r
@@ -61,11 +61,16 @@ runMSErobust <- function(OM = "1", MPs = NA, nsim = 200, proyears = 28,
   maxsims = 64, name = NULL, unique=TRUE, maxCrash = 10, saveMSE = TRUE, 
   savePack = FALSE) {
   
+  if (!snowfall::sfIsRunning()) {
+    message("Setting up parallel processing")
+	setup()
+  }
+  
   packets <- new("list")  # a list of completed MSE objects
   simsplit <- split(1:nsim, ceiling(seq_along(1:nsim)/maxsims))  # split the runs
-  
-  if (!snowfall::sfIsRunning()) stop("You must set up parallel processing to use runMSErobust. Use: setup()", call.=FALSE)
-  
+  message("Running a total of ", length(simsplit), " packets\n")
+  flush.console()  
+
   if (is.null(name)) {
     st <- as.numeric(regexpr(":", OM@Name)) + 1
     nd <- st + 3  # as.numeric(regexpr(' ', OM@Name))-1
@@ -75,39 +80,57 @@ runMSErobust <- function(OM = "1", MPs = NA, nsim = 200, proyears = 28,
   if (nchar(name) < 1)  name <- paste0("MSE_")
   
   if (unique) name <- paste0(name, "_", format(Sys.time(), "%H%M_%m%d%y"))
-  
+  stElap <- rep(NA, length(simsplit)) # store elapsed time 
   for (i in 1:length(simsplit)) {
+    message("Packet ", i, " of ", length(simsplit), " started")
+    flush.console()
     error <- 1
     crash <- 0
-    while (error == 1 & crash <= maxCrash) {
+	st <- Sys.time()
+    while (error == 1 & crash <= maxCrash) { 
       trialMSE <- try(runMSE(OM = OM, MPs = MPs, nsim = length(simsplit[[i]]), 
         proyears = proyears, interval = interval, pstar = pstar, 
         maxF = maxF, timelimit = timelimit, reps = reps, custompars = custompars, 
-        CheckMPs = CheckMPs, Hist, ntrials, fracD))
-      crash <- crash + 1
-      if (crash >= maxCrash) 
-        stop("Crashed too many times!")
+        CheckMPs = CheckMPs, Hist, ntrials, fracD))	
+      if (class(trialMSE) != "MSE") {
+	    crash <- crash + 1
+		print(warnings())
+		message("Packet ", i, " crashed. Trying again\n")
+	  }
+      if (crash >= maxCrash) stop("\nNumber of crashes exceeded 'maxCrash'\n", call.=FALSE)
       if (class(trialMSE) == "MSE") {
         packets[[i]] <- trialMSE
         fname <- paste0(name, "pack", i, ".rdata")
         if (savePack) {
           saveRDS(trialMSE, file = fname)
-          message(paste("Saving", fname, "to", getwd()))
+          message("Saving" , fname, " to ", getwd())
           flush.console()
-        }
+        }	
         error <- 0
         crash <- 0
       }
     }
-    message(paste("Packet", i, "of", length(simsplit), "complete"))
+    elapse <- Sys.time() - st 
+	stElap[i] <- elapse
+    message("Packet ", i, " of ", length(simsplit), " complete\n")
     flush.console()
+	eta <- round(mean(stElap, na.rm=TRUE) * length(simsplit) - sum(stElap, na.rm=TRUE), 2)
+	units <- attributes(elapse)$units
+	if (eta > 120 && units == "secs") {
+	  eta <- round(eta/60,2)
+	  units <- "mins"
+	}
+	if (i != length(simsplit))
+	  message("\nEstimated time to completion is: ", eta, " ", units, " (", format(st + eta, "%H:%M %x"), ")\n")
+	flush.console()
+	
   }
   if (i == 1) MSEobj <- packets[[1]]
   if (i > 1) MSEobj <- joinMSE(MSEobjs = packets)
   if (saveMSE) {
     fname <- paste0(name, ".rdata")
     saveRDS(MSEobj, file = fname)
-    message(paste("Saving", fname, "to", getwd()))
+    message("Saving ", fname, " to ", getwd())
     flush.console()
   }
   MSEobj
