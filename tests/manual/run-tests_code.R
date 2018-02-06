6d8da0073bfd8990aee8edbd4a37b38290bd6dde
Adrian Hordyk <ar.hordyk@gmail.com>
2018-02-06 15:39:22 -0800

    add new tests

---
+++ tests/manual/run-tests_code.R
@@ -0,0 +1,9 @@
+
+library(testthat)
+
+testthat::test_dir('tests/manual/test-code')
+                   
+
+source("tests/manual/run-coverage_code.R")
+
+
---
+++ tests/manual/test-code/test-Data_Functions.R
@@ -0,0 +1,29 @@
+testthat::context("Data_Functions")
+
+DLMextra()
+library(DLMextra)
+Dat <- avail(Data)
+Dat <- Dat[!Dat %in% c("SimulatedData", "Simulation_1")]
+
+output <- avail('Output')
+                    
+for (dat in Dat) {
+  testthat::test_that(paste("runMP, Can, Cant, Needed, Sense, TAC, work with ", dat), {
+    datobj <- get(dat)
+    cans <- Can(datobj)
+    cants <- Cant(datobj)
+    avails <- avail("MP")
+    testthat::expect_error(out <<- runMP(datobj, silent=TRUE), NA)
+    testthat::expect_true(length(out@MPs) == length(cans))
+    testthat::expect_true(length(avails) - nrow(cants) == length(cans))
+    testthat::expect_true(length(Needed(datobj)) == nrow(cants))
+    testthat::expect_error(Sense(out, out@MPs[1]), NA)
+    testthat::expect_error(TAC(datobj), NA)
+    
+  })
+}
+ 
+
+  
+                    
+
