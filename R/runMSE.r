7df98c7da0deddd3cac5c9f43cd3affffbe48cc0
Adrian Hordyk <ar.hordyk@gmail.com>
2018-04-11 15:42:34 -0700

    more CAL robustness

--- R/runMSE.r
+++ R/runMSE.r
@@ -1069,6 +1069,19 @@ runMSE_int <- function(OM = DLMtool::testOM, MPs = c("AvC","DCAC","FMSYref","cur
                            t0array[,nyears + yind,drop=FALSE], LenCV, truncSD=2)
         CAL <- aperm(array(as.numeric(unlist(tempSize, use.names=FALSE)), dim=c(length(yind), length(CAL_binsmid), nsim)), c(3,1,2))
         
+        # check CAL 
+        chkCAL <- apply(CAL, c(1,2), sum)
+        if (any(chkCAL==0)) {
+          for (ss in 1:nrow(chkCAL)) {
+            for (yy in 1:ncol(chkCAL)) {
+              if (chkCAL[ss,yy]==0) {
+                bn <- ceiling(c(0.5*nCALbins, 0.5*nCALbins+1))
+                CAL[ss,yy,bn] <- 1 # if CAL is empty - put 1 individual in two middle bins - for robustness
+              }
+            }
+          }
+        }
+        
         # calculate LFC - approx 5th percentile 
         LFC <- unlist(lapply(tempSize, function(x) getfifth(x[nrow(x), ], CAL_binsmid)))
         LFC[is.na(LFC)] <- 1
