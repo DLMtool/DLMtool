3195abe91d708a2cf1d49ace874f6c41ab773dfe
Adrian Hordyk <ar.hordyk@gmail.com>
2018-04-17 09:56:52 -0700

    fixes #168 [skip ci]

--- R/OM_init_doc.R
+++ R/OM_init_doc.R
@@ -546,15 +546,15 @@ OMdoc <- function(OM=NULL, rmd.source=NULL, overwrite=FALSE, out.file=NULL,
     fileName <- OM@Name
     fileName <- gsub(" ", "", gsub("[[:punct:]]", "", fileName))
     if (nchar(fileName)>15) fileName <-  substr(fileName, 1, 15)
-      
-    if (file.exists(paste0(file.path(dir, 'build/', fileName, '.dat')))) {
+    
+    if (file.exists(paste0(file.path(dir, 'build/', paste0(fileName, '.dat'))))) {
       # OM has been documented before - check if it has changed
       testOM <- readRDS(file.path(dir,paste0("build/", fileName, '.dat')))
       if (class(testOM) == 'OM') {
         # check if OM has changed 
         changed <- rep(FALSE, length(slotNames(OM)))
         for (sl in seq_along(slotNames(OM))) {
-      
+          
           oldOM <- slot(OM, slotNames(OM)[sl])
           newOM <- slot(testOM, slotNames(OM)[sl])
           if (class(oldOM) !='character') {
@@ -583,7 +583,7 @@ OMdoc <- function(OM=NULL, rmd.source=NULL, overwrite=FALSE, out.file=NULL,
         file.remove(file.path(dir,paste0('build/',fileName, '.dat')))
         runSims <- TRUE
       }
-     
+      
     } else{
       saveRDS(OM, file=file.path(dir,paste0('build/', fileName, '.dat')))
       runSims <- TRUE
@@ -717,7 +717,7 @@ OMdoc <- function(OM=NULL, rmd.source=NULL, overwrite=FALSE, out.file=NULL,
     message("\n\nRendering markdown document as ", RMDfileout)
     EffYears <- seq(from=(OM@CurrentYr -  OM@nyears + 1), to=OM@CurrentYr, length.out=length(OM@EffYears))
     EffYears <- round(EffYears,0)
-    Effvals <- data.frame(EffYears=EffYears, EffLower=OM@EffLower, EffUpper=OM@EffUpper)
+    Effvals <- data.frame(EffYears=EffYears, EffLower=signif(OM@EffLower,3), EffUpper=signif(OM@EffUpper,3))
     params <- list(OM=OM, Pars=Pars, Effvals=Effvals, out=out)
     rmarkdown::render(input=RMDfile, output_file=RMDfileout, output_format=output, 
                       output_dir=dir, param=params, quiet=quiet)
@@ -894,7 +894,12 @@ writeSection <- function(class=c("Intro", "Stock", "Fleet", "Obs", "Imp", "Refer
             valtext <- paste0("Specified in cpars: ", "<span style='color:", color, "'>", " ", trimws(val), "</span>", "\n\n")
           } else {
             if (length(Pars[[sl]])>0) {
-              val <- range(Pars[[sl]])
+              if (length(Pars[[sl]])==1) val <- (Pars[[sl]])
+              if (length(Pars[[sl]])>1) {
+                if (all(Pars[[sl]] == mean(Pars[[sl]]))) {
+                  val <- mean(Pars[[sl]])
+                } else  val <- range(Pars[[sl]])
+              }
             } else {
               val <- slot(OM, sl)  
             }
@@ -950,7 +955,7 @@ writeSection <- function(class=c("Intro", "Stock", "Fleet", "Obs", "Imp", "Refer
             cat("</style>\n", append=TRUE, file=RMDfile, sep="")
             
             cat("```{r, echo=FALSE, results='asis'}\n", append=TRUE, file=RMDfile, sep="")
-            cat("knitr::kable(signif(Effvals,3), format='markdown', caption='')\n", append=TRUE, file=RMDfile, sep="")
+            cat("knitr::kable(Effvals, format='markdown', caption='')\n", append=TRUE, file=RMDfile, sep="")
             cat("```\n\n", append=TRUE, file=RMDfile, sep="")
             
           }
