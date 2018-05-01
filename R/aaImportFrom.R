6523501a8298d8aad7b2a4750c647cb6915aa679
Adrian <ar.hordyk@gmail.com>
2018-05-01 15:49:13 -0700

    begin documenting MPs [skip ci]

---
+++ R/aaImportFrom.R
@@ -0,0 +1,27 @@
+#' @importFrom abind abind
+#' @importFrom devtools install_github
+#' @importFrom dplyr  %>%  select mutate
+#' @importFrom ggplot2 ggplot aes geom_rect geom_point xlim ylim xlab ylab theme theme_classic labs ggplotGrob
+#' @importFrom ggrepel geom_text_repel
+#' @importFrom graphics abline arrows axis barplot boxplot contour hist identify layout legend
+#'  lines matplot mtext par plot plot.new points polygon segments text title text
+#' @importFrom grDevices col2rgb colorRampPalette dev.off jpeg rainbow rgb xy.coords
+#' @importFrom gridExtra arrangeGrob
+#' @importFrom grid unit.c unit grid.newpage grid.draw
+#' @importFrom kableExtra cell_spec kable_styling column_spec add_header_above 
+#' @importFrom knitr kable
+#' @importFrom MASS mvrnorm kde2d
+#' @importFrom methods getClassDef getSlots .hasSlot new show slot slot<- slotNames
+#' @importFrom mvtnorm rmvnorm
+#' @importFrom parallel detectCores 
+#' @importFrom snowfall sfClusterEval sfInit sfExportAll sfIsRunning sfExport sfSapply sfLibrary
+#' @importFrom stats acf approx coef dbeta density dnorm dlnorm lm loess loess.smooth
+#' median nlm optim optimise optimize plogis pnorm predict qlnorm quantile rbeta
+#' rlnorm rmultinom rnorm runif sd
+#' @importFrom utils  browseURL capture.output combn flush.console packageVersion ls.str lsf.str read.csv read.csv2
+#' 
+NULL
+
+
+
+
---
+++ man-roxygen/MPoutput.R
@@ -0,0 +1,5 @@
+#' @return An object of class \code{\link[=Rec-class]{Rec}} with the \code{TAC} slot populated with a numeric vector of length \code{reps}
+#' 
+
+
+
