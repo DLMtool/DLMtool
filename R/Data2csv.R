70c4f2070d07fd5a397c9cb2fd80faece446774f
tcarruth <tomsinbox@gmail.com>
2018-11-16 14:56:25 -0800

    debugged Data2csv

--- R/Data2csv.R
+++ R/Data2csv.R
@@ -10,7 +10,7 @@
 #' @param keepNAs Boolean. Should slots with NAs still be written to the data file. 
 #' @author T. Carruthers
 #' @export
-Data2csv <- function(Data, file=NULL, simno = 1, overwrite=F, keepNAs=T) {
+Data2csv<-function(Data, file=NULL, simno = 1, overwrite=F, keepNAs=T) {
   
   if(class(Data)!='Data') stop("First argument 'Data' not an object of class 'Data' ")
   
@@ -35,7 +35,7 @@ Data2csv <- function(Data, file=NULL, simno = 1, overwrite=F, keepNAs=T) {
   
   if(file.exists(file)){
     if(!overwrite){
-      ANSWER <- readline("This file already exists. Is it ok to continue and overwrite it (y/n) ?")
+      ANSWER<- readline("This file already exists. Is it ok to continue and overwrite it (y/n) ?")
       ## a better version would check the answer less cursorily, and
       ## perhaps re-prompt
       if (substr(ANSWER, 1, 1) == "n"){
@@ -64,24 +64,96 @@ Data2csv <- function(Data, file=NULL, simno = 1, overwrite=F, keepNAs=T) {
     }
   }
   
+  lexicon<-t(matrix(
+    c("Name","Name",
+      "Common_Name", "Common Name",
+      "Species", "Species",
+      "Region",  "Region",
+      "Year","Year", 
+      "Cat", "Catch",
+      "Ind", "Abundance index",
+      "Rec", "Recruitment",
+      "t","Duration t",
+      "AvC","Average catch over time t",
+      "Dt","Depletion over time t",
+      "Mort","M",
+      "FMSY_M","FMSY/M",
+      "BMSY_B0","BMSY/B0",
+      "L50","Length at 50% maturity",
+      "L95","Length at 95% maturity",
+      "ML", "Mean length",
+      "Lbar", "Mean length",
+      "Lc", "Modal length Lc",
+      "LFC","Length at first capture",
+      "LFS","Length at full selection",
+      "CAA", "CAA",
+      "Dep","Current stock depletion",
+      "Abun","Current stock abundance",
+      "SpAbun","Current spawning stock abundance",
+      "vbK","Von Bertalanffy K parameter",
+      "vbLinf","Von Bertalanffy Linf parameter",
+      "vbt0","Von Bertalanffy t0 parameter",
+      "LenCV","CV of length-at-age",
+      "wla","Length-weight parameter a",
+      "wlb","Length-weight parameter b",
+      "steep","Steepness",
+      "sigmaR","sigmaR",
+      "CV_Cat","CV Catch",
+      "CV_Dt","CV Depletion over time t",
+      "CV_AvC","CV Average catch over time t",
+      "CV_Ind","CV Abundance index",
+      "CV_Mort","CV M",
+      "CV_FMSY_M","CV FMSY/M",
+      "CV_BMSY_B0","CV BMSY/B0",
+      "CV_Dep","CV current stock depletion",
+      "CV_Abun","CV current stock abundance",
+      "CV_vbK","CV von B. K parameter",
+      "CV_vbLinf","CV von B. Linf parameter",
+      "CV_vbt0","CV von B. t0 parameter",
+      "CV_L50","CV Length at 50% maturity",
+      "CV_LFC","CV Length at first capture",
+      "CV_LFS","CV Length at full selection",
+      "CV_wla","CV Length-weight parameter a",
+      "CV_wlb","CV Length-weight parameter b",
+      "CV_steep","CV Steepness",
+      "sigmaL","Sigma length composition",
+      "MaxAge","Maximum age",
+      "Units", "Units",
+      "Ref","Reference OFL",
+      "Ref_type", "Reference OFL type",
+      "Cref","Cref",
+      "Iref","Iref",
+      "Bref","Bref",
+      "CV_Cref","CV Cref",
+      "CV_Iref","CV Iref",
+      "CV_Bref","CV Bref",
+      "CV_Rec","CV Rec",
+      "MPrec","MPrec",
+      "MPeff","MPeff",
+      "LHYear","LHYear",
+      "nareas","nareas"),
+      nrow=2))
+ 
+  
   for(i in 1:ns){
     obj<-slot(Data,slots[i])
+    lex<-lexicon[match(slots[i],lexicon[,1]),2]
     if(class(obj)!="list" & class(obj)!="data.frame"){
       allNA<-sum(!is.na(obj))==0
       if(!(allNA & !keepNAs)){ # write NA values
         if(slottest(obj)){ # is the slot empty or all NAs?
-          write(paste(c(slots[i],"NA"),collapse=","),file,1,append=appendy[i])
+          write(paste(c(lex,"NA"),collapse=","),file,1,append=appendy[i])
         }else{
           if(is.null(dim(obj))){  # vector or single value
-            write(paste(c(slots[i],obj),collapse=","),file,1,append=appendy[i])
+            write(paste(c(lex,obj),collapse=","),file,1,append=appendy[i])
           
           }else if(length(dim(obj))==2){ # a matrix (time series)
-            write(paste(c(slots[i],obj[simno,]),collapse=","),file,1,append=appendy[i])
+            write(paste(c(lex,obj[simno,]),collapse=","),file,1,append=appendy[i])
            
           }else if(length(dim(obj))==3){ # 3d array of composition data (CAL, CAA)
              ny<-dim(obj)[2]
             for(yy in 1:ny){
-              write(paste(c(paste(slots[i],yrs[yy]),obj[simno,yy,]),collapse=","),file,1,append=appendy[i])
+              write(paste(c(paste(lex,yrs[yy]),obj[simno,yy,]),collapse=","),file,1,append=appendy[i])
             }
           } 
         } # end of slottest
