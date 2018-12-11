30e70d3e2c23fcb646424c21de2edeb05e6f2eee
Adrian <ar.hordyk@gmail.com>
2018-12-11 11:39:18 -0800

    update userguide

---
+++ tests/manual/test_dev/popdyn_tests.R
@@ -0,0 +1,119 @@
+
+# Scripts to check for inconsistencies in the internal population dynamics 
+
+library(DLMtool)
+
+# Test Effort calculation with fixed TAC 
+
+# set equilbrium conditions 
+OM <- testOM 
+OM <- tinyErr(OM)
+OM@D <- c(0.4, 0.5)
+OM@h <- c(0.6, 0.6)
+OM@Vmaxlen <- c(1,1)
+OM@EffLower <- c(0,1,1,1)
+OM@EffUpper <- OM@EffLower
+OM@nyears <- 100
+OM@nsim <- 5
+OM@proyears <- 30
+OM@interval <- 10
+
+curCatch <- function(x, Data, ...) {
+  Rec <- new("Rec")
+  Rec@Spatial <- c(0,1)
+  Rec@Allocate <- 0
+  Rec@TAC <- Data@Cat[x, Data@Year == Data@LHYear]
+  Rec
+}
+class(curCatch) <- 'MP'
+
+# check effort if Area 1 is closed - should go up with current catch 
+devtools::load_all()
+
+OM@Cbiascv <- 0
+OM@Cobs <- c(0,0)
+OM@qinc <- c(0,0)
+OM@qcv <- c(0,0)
+OM@Prob_staying <- c(0.99,0.99)
+OM@Size_area_1 <- OM@Frac_area_1 <- c(0.6,0.6)
+MSE <- runMSE(OM, MP=c('curCatch'))
+
+MSE@Effort[1,1,]
+
+plot(MSE@Effort[1,1,], type="l")
+
+plot(MSE@B_BMSY[1,1,], type="l")
+plot(MSE@F_FMSY[1,1,], type="l")
+
+
+HistCatches <- apply(MSE@CB_hist, c(1,3), sum)
+FutureCatch <- MSE@C
+HistCatches[,OM@nyears]
+FutureCatch[,1,1]
+
+y <- 1:10
+qinc <- 1
+(1/((1 + qinc/100)^y))
+
+
+
+
+
+AllCatch <- cbind(HistCatches, FutureCatch[,2,])
+AllCatch[,OM@nyears:(OM@nyears+1)]
+
+matplot(t(AllCatch), type="l")
+
+Cobs[,OM@nyears]
+
+# Check refY is calculated correctly 
+
+OM <- testOM
+
+# OM <- tinyErr(OM)
+OM@D <- c(0.4, 0.5)
+OM@h <- c(0.6, 0.6)
+OM@Vmaxlen <- c(1,1)
+OM@nsim <- 5
+OM@proyears <- 100
+
+RefYMP <- function(x, Data, ...) {
+  Rec <- new("Rec")
+  Rec@TAC <- Data@OM$RefY[x]
+  Rec
+}
+class(RefYMP) <- 'MP'
+
+MSYMP <- function(x, Data, ...) {
+  Rec <- new("Rec")
+  Rec@TAC <- Data@OM$MSY[x]
+  Rec
+}
+class(MSYMP) <- 'MP'
+
+MSE <- runMSE(OM, MP=c('RefYMP', 'MSYMP', 'FMSYref'))
+
+par(mfrow=c(2,3))
+RefC <- MSE@C/MSE@OM$RefY
+matplot(t(RefC[,1,]), type="l", ylim=c(0,1))
+matplot(t(RefC[,2,]), type="l", ylim=c(0,1))
+matplot(t(RefC[,3,]), type="l", ylim=c(0,1))
+
+matplot(t(MSE@B_BMSY[,1,]), type="l", ylim=c(0, 2))
+matplot(t(MSE@B_BMSY[,2,]), type="l", ylim=c(0, 2))
+matplot(t(MSE@B_BMSY[,3,]), type="l", ylim=c(0, 2))
+
+
+MSE@OM$RefY
+MSE@OM$MSY
+
+
+
+
+
+
+MSE <- runMSE(OM, MP='MSYMP')
+par(mfrow=c(2,1))
+RefC <- MSE@C/MSE@OM$RefY
+matplot(t(RefC[,1,]), type="l", ylim=c(0,1))
+matplot(t(MSE@B_BMSY[,1,]), type="l", ylim=c(0, 2))
