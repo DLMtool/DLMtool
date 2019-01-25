ae8645d7462e94b517c11726313607496084c8c8
Adrian <ar.hordyk@gmail.com>
2019-01-25 15:46:34 -0800

    Update run-tests-code.R

--- tests/manual/run-tests-code.R
+++ tests/manual/run-tests-code.R
@@ -1,8 +1,9 @@
 library(DLMtool)
 
-testthat::test_dir('tests/manual/test-code')
+options(testthat.output_file = "test-out.xml")
+testthat::test_dir('tests/manual/test-code', reporter = "junit")
 
-# testthat::test_file("tests/manual/test-code/test-Data_Functions.R") # ok
+# testthat::test_file("tests/manual/test-code/test-Data_Functions.R", reporter = "junit") # ok
 
 # testthat::test_file("tests/manual/test-code/test-Data_Plotting.R") #  ok
 # 
