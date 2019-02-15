3b6902ac7abb52f6a128ad5f520b2b1f6b2b43d1
Adrian <ar.hordyk@gmail.com>
2019-02-15 10:03:04 -0800

    update PMtables

--- R/PMtable.R
+++ R/PMtable.R
@@ -4,6 +4,8 @@
 #' @param MSE An object of class 'MSE'
 #' @param ... PM objects to be used as performance limits. Characters (i.e names of PM objects)
 #' @param Pthresh Minimum probability threshold
+#' @param Labels Optional named list specifying new labels for MPs. For example: `Labels = list(AvC="Average Catch", CC1="Constant Catch")`
+#' @param FeaseMPs Optional. Character vector of MP names that are considered feasible. e.g. the output from `Fease()`
 #' @param out.file Name of the output file. If none provided, output file will be named 'PerfLimTable' 
 #' @param output_format Output file format. Currently only 'html_document' is supported
 #' @param openFile Logical. Should the file be opened in browser?
@@ -19,7 +21,8 @@
 #' @return Invisibly returns names of MPs that pass all performance limits
 #' @export
 #'
-PMLimit <- function(MSE, ..., Pthresh=NULL, out.file=NULL,
+PMLimit <- function(MSE, ..., Pthresh=NULL, Labels=NULL, FeaseMPs=NULL,
+                    out.file=NULL,
                       output_format="html_document", openFile=TRUE,
                       quiet=TRUE, dir=NULL, RMDfile=NULL, font_size=14,
                       full_width=TRUE, enableSearch=TRUE, PMlist=NULL, build=TRUE) {
@@ -58,7 +61,29 @@ PMLimit <- function(MSE, ..., Pthresh=NULL, out.file=NULL,
   ind <- match(df$MP, types[,1])
   df$Type <- types[ind,2]
   
+  labels <- MSE@MPs
+  if (class(Labels) == "list") {
+    repnames <- names(Labels)
+    invalid <- repnames[!repnames %in% labels]
+    if (length(invalid >0)) {
+      warning("Labels: ", paste(invalid, collapse=", "), " are not MPs in MSE")
+      Labels[invalid] <- NULL
+      repnames <- names(Labels)
+    }
+    labels[labels %in% repnames] <- Labels %>% unlist()
+  }
   
+  
+  df$MP <- labels[match(df$MP,MSE@MPs)]
+  df$Feasible <- TRUE
+  if(!is.null(FeaseMPs)) {
+    if(!class(FeaseMPs) == "character") stop("FeaseMPs must be character vector", call.=FALSE)
+    df$Feasible <- rep(MSE@MPs %in% FeaseMPs, each=2)
+  }
+  df$Feasible[df$Feasible != TRUE] <- "No"
+  df$Feasible[df$Feasible == TRUE] <- "Yes"
+  
+ 
   if (output_format == "html_document") {
     ext <- '.html'
   } else if (output_format == "pdf_document") {
@@ -85,7 +110,7 @@ PMLimit <- function(MSE, ..., Pthresh=NULL, out.file=NULL,
   if(is.null(RMDfile))
     RMDfile <- file.path(system.file(package = "DLMtool"), 'PLimitTable.Rmd')
 
-  out <- df %>% filter(min >= Pthresh) %>% dplyr::select(MP) %>% unique()
+  out <- df %>% filter(min >= Pthresh & Feasible=="Yes") %>% dplyr::select(MP) %>% unique()
   if (build) {
     if (file.exists(RMDfileout)) unlink(RMDfileout)
     rmarkdown::render(input=RMDfile, output_file=RMDfileout, output_format=output_format,
@@ -105,25 +130,35 @@ PMLimit <- function(MSE, ..., Pthresh=NULL, out.file=NULL,
 #'
 #' @param MSE An object of class 'MSE'
 #' @param ... PM objects to be used as performance metrics. Characters (i.e names of PM objects)
-#' @param out.file Name of the output file. If none provided, output file will be named 'PerfLimTable' 
+#' @param Labels Optional named list specifying new labels for MPs. For example: `Labels = list(AvC="Average Catch", CC1="Constant Catch")`
+#' @param out.file Name of the output file. If none provided, output file will be named 'PerfObjTable' 
 #' @param output_format Output file format. Currently only 'html_document' is supported
 #' @param openFile Logical. Should the file be opened in browser?
 #' @param quiet Logical. An option to suppress printing of the pandoc command line.
 #' @param dir Optional. Directory for output file. Default is working directory.
 #' @param RMDfile Optional. RMD template file
 #' @param font_size Numeric. Font size for text in the table
+#' @param use.colors Logical. Color scale the probability text?
+#' @param cols Optional character vector of colors for probability text
+#' @param show.legend Logical. Show the legend?
 #' @param full_width Logical. Should table be full width?
 #' @param enableSearch Logical. Should search be enabled in the html table?
 #' @param PMlist Optional. List of PM names.
 #' @param build Logical. Build the html table?
-#'
+#' @param cex.tex Size of legend text
+#' @param inc.title Logical. Include title for legend?
+#' @param title Title for the legend
 #' @return Nothing is returned. 
 #' @export
 #'
-PMObj <- function(MSE, ..., out.file=NULL,
+#'
+PMObj <- function(MSE, ..., Labels=NULL, out.file=NULL,
                   output_format="html_document", openFile=TRUE,
-                  quiet=TRUE, dir=NULL, RMDfile=NULL, font_size=14, 
-                  full_width=TRUE, enableSearch=TRUE, PMlist=NULL, build=TRUE) {
+                  quiet=TRUE, dir=NULL, RMDfile=NULL, font_size=14,
+                  use.colors=TRUE,
+                  cols=NULL, show.legend=TRUE,
+                  full_width=TRUE, enableSearch=TRUE, PMlist=NULL, build=TRUE,
+                  cex.tex=0.75, inc.title=TRUE, title="Legend") {
   
   if (class(MSE) != 'MSE') stop("Object is not class 'MSE'", call. = FALSE)
   nMPs <- MSE@nMPs
@@ -157,6 +192,20 @@ PMObj <- function(MSE, ..., out.file=NULL,
   df$Type <- NA
   ind <- match(df$MP, types[,1])
   df$Type <- types[ind,2]
+  
+  labels <- MSE@MPs
+  if (class(Labels) == "list") {
+    repnames <- names(Labels)
+    invalid <- repnames[!repnames %in% labels]
+    if (length(invalid >0)) {
+      warning("Labels: ", paste(invalid, collapse=", "), " are not MPs in MSE")
+      Labels[invalid] <- NULL
+      repnames <- names(Labels)
+    }
+    labels[labels %in% repnames] <- Labels %>% unlist()
+  }
+  
+  df$MP <- labels[match(df$MP,MSE@MPs)]
 
   if (output_format == "html_document") {
     ext <- '.html'
@@ -176,9 +225,14 @@ PMObj <- function(MSE, ..., out.file=NULL,
   if (is.null(dir)) dir <- getwd()
   RMDfileout <- file.path(dir, out.file)
   
+  if (is.null(cols)) {
+    colsfun <- colorRampPalette(c("forestgreen", "orange", "red"))
+    cols <- rev(colsfun(5))
+  }
   params <- list(df=df, runPM=runPM, Name=MSE@Name, font_size=font_size,
                  full_width=full_width, output_format=output_format,
-                 enableSearch=enableSearch)
+                 enableSearch=enableSearch, cols=cols, use.colors=use.colors,
+                 show.legend=show.legend)
   knitr::knit_meta(class=NULL, clean = TRUE)
   
   if(is.null(RMDfile))
@@ -196,5 +250,7 @@ PMObj <- function(MSE, ..., out.file=NULL,
   }  
 
 }
+
+
 
 
