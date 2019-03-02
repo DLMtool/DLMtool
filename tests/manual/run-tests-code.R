ad85c25efb1586929bb33893a7328344a2361961
Adrian <ar.hordyk@gmail.com>
2019-03-01 16:07:51 -0800

    update tests

--- tests/manual/run-tests-code.R
+++ tests/manual/run-tests-code.R
@@ -3,39 +3,39 @@ library(DLMtool)
 options(testthat.output_file = "test-out.xml")
 testthat::test_dir('tests/manual/test-code', reporter = "junit")
 
-testthat::test_file("tests/manual/test-code/test-Data_Functions.R") #
+# testthat::test_file("tests/manual/test-code/test-Data_Functions.R") # Ok 
 
-testthat::test_file("tests/manual/test-code/test-Data_Plotting.R") #
+# testthat::test_file("tests/manual/test-code/test-Data_Plotting.R") # Ok
 
-testthat::test_file("tests/manual/test-code/test-Fease_Functions.R") #
+# testthat::test_file("tests/manual/test-code/test-Fease_Functions.R") # Ok 
 
-testthat::test_file("tests/manual/test-code/test-Import_Data.R") #
+# testthat::test_file("tests/manual/test-code/test-Import_Data.R") # Ok
 
-testthat::test_file("tests/manual/test-code/test-MPs.R") #
+# testthat::test_file("tests/manual/test-code/test-MPs.R") # Ok
 
-testthat::test_file("tests/manual/test-code/test-MSE_functions.R") #
+# testthat::test_file("tests/manual/test-code/test-MSE_functions.R") # Ok
 
-testthat::test_file("tests/manual/test-code/test-MSE_Plotting.R") #
+# testthat::test_file("tests/manual/test-code/test-MSE_Plotting.R") # Ok
 
-testthat::test_file("tests/manual/test-code/test-OM_functions.R") #
+# testthat::test_file("tests/manual/test-code/test-OM_functions.R") # Ok 
 
-testthat::test_file("tests/manual/test-code/test-OM_init_doc.R") #
+# testthat::test_file("tests/manual/test-code/test-OM_init_doc.R") # Ok
 
-testthat::test_file("tests/manual/test-code/test-OM_Plotting.R") # takes a while - some fail
+# testthat::test_file("tests/manual/test-code/test-OM_Plotting.R") # Ok
 
 testthat::test_file("tests/manual/test-code/test-runMSE.R") # to check still
 
-testthat::test_file("tests/manual/test-code/test-slotDescription.R") #
+# testthat::test_file("tests/manual/test-code/test-slotDescription.R") # Ok
 
-testthat::test_file("tests/manual/test-code/test-cpars.R") #
+# testthat::test_file("tests/manual/test-code/test-cpars.R") # Ok 
 
-testthat::test_file("tests/manual/test-code/test-PMobjects.R") #
+# testthat::test_file("tests/manual/test-code/test-PMobjects.R") # Ok 
 
-testthat::test_file("tests/manual/test-code/test-RealIndices.R") #
+# testthat::test_file("tests/manual/test-code/test-RealIndices.R") # Ok
 
-testthat::test_file("tests/manual/test-code/test-Data2csv.R") #
+# testthat::test_file("tests/manual/test-code/test-Data2csv.R") # Ok
 
-testthat::test_file("tests/manual/test-code/test-checkPopdyn.R") #
+# testthat::test_file("tests/manual/test-code/test-checkPopdyn.R") # Ok
 
 
 
--- tests/manual/test-code/build/readme.txt
+++
@@ -1,4 +0,0 @@
-This directory was created by DLMtool function OMdoc
-
-Files in this directory are used to generate the OM report.
-
