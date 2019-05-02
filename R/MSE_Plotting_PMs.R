99c80841147e9dcb3582787feef95fc14b088820
Adrian <ar.hordyk@gmail.com>
2019-05-02 13:26:29 -0700

    Update MSE_Plotting_PMs.R

--- R/MSE_Plotting_PMs.R
+++ R/MSE_Plotting_PMs.R
@@ -202,7 +202,7 @@ TradePlot <- function(MSEobj, ..., Lims=c(0.2, 0.2, 0.8, 0.8),
     if (Col == "Class") {
       plots[[pp]] <-  plots[[pp]] +  ggplot2::scale_colour_manual(values=cols)
     } else if (Col == "MP") {
-      plots[[pp]] <-  plots[[pp]] +  ggplot2::guides(color=FALSE)
+      plots[[pp]] <-  plots[[pp]] +  ggplot2::scale_colour_manual(values=cols) +  ggplot2::guides(color=FALSE)
     }
     
     if (!is.null(Title)) 
