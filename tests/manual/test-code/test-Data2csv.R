aa10df96b864c3504b21e012efc3d59861c47465
Adrian <ar.hordyk@gmail.com>
2019-08-28 16:07:33 -0700

    update tests

--- tests/manual/test-code/test-Data2csv.R
+++ tests/manual/test-code/test-Data2csv.R
@@ -10,7 +10,7 @@ for (dat in dats) {
     Data2csv(get(dat), file, simno = sim,overwrite=T)
     readDat <- new("Data", file)
     for (sl in slotNames('Data')) {
-      if (!sl %in% c("TAC", "Sense", "MPrec")) {
+      if (!sl %in% c("TAC", "Sense", "MPrec", "PosMPs", "MPs")) {
         orig <- slot(get(dat), sl)
         if (class(orig) == "integer") orig <- as.numeric(orig)
         read <- slot(readDat, sl)
@@ -22,8 +22,8 @@ for (dat in dats) {
         } else {
           if (class(orig)=="matrix") {
             nonna <- which(!is.na(orig[sim,]))
-            
-            testthat::expect_equal(orig[sim,nonna], read[sim,nonna])
+            if (length(nonna)>0)
+              testthat::expect_equal(orig[sim,nonna], read[sim,nonna])
           }
           if (class(orig)=="numeric") testthat::expect_equal(orig[sim], read[sim])
         }
--- tests/manual/test-code/test-Import_Data.R
+++ tests/manual/test-code/test-Import_Data.R
@@ -1,6 +1,12 @@
 testthat::context("Import Data objects")
 
-fls <- list.files("../../../../DLMDev/DLMtool_Data/Data")
+
+if (basename(getwd()) == "DLMtool") {
+  fls <- list.files("../DLMDev/DLMtool_Data/Data") 
+} else {
+  fls <- list.files("../../../../DLMDev/DLMtool_Data/Data")
+}
+
 for (fl in fls) {
   testthat::test_that(paste("new('Data') works with ", fl), {
     testthat::expect_error(Data <- new("Data", file.path("../../../../DLMDev/DLMtool_Data/Data", fl)), NA)
